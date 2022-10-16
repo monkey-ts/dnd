b3395c54a512ede5b9e7c1de0379d2a5c2c36d3e
monkey-ts <m.inifiedcode@gmail.com>
2022-10-16 13:22:35 -0500

    Format with Prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import NavBar from "../components/Nav/Bar"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import NavBar from "../components/Nav/Bar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <NavBar />
-    <Component {...pageProps} />
+      <NavBar />
+      <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
